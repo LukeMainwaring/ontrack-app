c28667b21a63539d667158f6f2d2a9312b64b9d8
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-01-12 23:27:30 -0500

    set up base url for production

--- src/api/ontrack.js
+++ src/api/ontrack.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+let apiUrl = '';
+if (process.env.NODE_ENV === 'development') {
+  apiUrl = 'http://8fe804c6.ngrok.io';
+} else {
+  // Production server
+  apiUrl =
+    'http://ontrackserver-env.pikgvbwvcg.us-east-1.elasticbeanstalk.com/';
+}
+
+// Uncomment this to connect local app to production server
+apiUrl = 'http://ontrackserver-env.pikgvbwvcg.us-east-1.elasticbeanstalk.com/';
+
 export default axios.create({
-  baseURL: 'http://baee6326.ngrok.io'
+  baseURL: apiUrl
 });
